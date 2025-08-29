69c375d12cc65724074daef1cfba0ba0872642b1
Pranshu Patel <pranshuvramani@gmail.com>
2025-08-29 21:58:10 +0530

    1.0.2 vercel

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+    import axios from "axios";
 
-// 1. Get the base URL using Vite's syntax
-const API_URL = import.meta.env.VITE_API_URL;
+    // 1. Get the base URL using Vite's syntax
+    const API_URL = import.meta.env.VITE_API_URL;
 
-// 2. Create a global Axios instance
-const api = axios.create({
-  baseURL: API_URL,
-});
+    // 2. Create a global Axios instance
+    const api = axios.create({
+    baseURL: API_URL,
+    });
 
-// 3. Export the instance
-export default api;
+    // 3. Export the instance
+    export default api;
