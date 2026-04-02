237cc9907268f2e984e2c10405f3cf0136c652d6
Pranshu Patel <pranshuvramani@gmail.com>
2026-04-03 02:19:03 +0530

    2.2.1x1 bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -15,11 +14,15 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("@radix-ui")) return "radix-ui";
-            if (id.includes("lucide-react")) return "lucide-react";
-            if (id.includes("react-router-dom") || id.includes("@remix-run") || id.includes("react-router")) return "router";
-            if (id.includes("@reduxjs") || id.includes("react-redux")) return "redux";
-            if (id.includes("react") || id.includes("react-dom")) return "react-core";
+            // Grouping React, Router, and Redux together to ensure hooks are available
+            if (
+              id.includes("react") || 
+              id.includes("react-dom") || 
+              id.includes("react-router") || 
+              id.includes("@reduxjs")
+            ) {
+              return "framework";
+            }
             return "vendor";
           }
         },
