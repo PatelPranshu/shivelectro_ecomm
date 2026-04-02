59cb0aa9e685f540252dd7fced715d41794ab8df
Pranshu Patel <pranshuvramani@gmail.com>
2026-04-03 02:30:02 +0530

    2.2.1x3 bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,24 +11,7 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // Grouping React, Router, and Redux together to ensure hooks are available
-            if (
-              id.includes("react") || 
-              id.includes("react-dom") || 
-              id.includes("react-router") || 
-              id.includes("@reduxjs")
-            ) {
-              return "framework";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
+    // Removing manualChunks to ensure React hooks are correctly linked across all dependencies
+    chunkSizeWarningLimit: 1500,
   },
 });
